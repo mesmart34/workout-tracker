a931b84f8e4ef15d07b05e610764e12bce70b4a1
EkimovAA <EkimovAA@ugsk.ru>
2025-04-30 02:33:38 +0500

    awdwadwada

---
+++ WorkoutTracker/Components/Pages/EquipmentList.razor.cs
@@ -0,0 +1,67 @@
+using Microsoft.AspNetCore.Components;
+using Radzen;
+using Radzen.Blazor;
+using WorkoutTracker.Application.Service;
+using WorkoutTracker.Common;
+using WorkoutTracker.Components.Shared;
+using WorkoutTracker.Domain.Entities;
+
+namespace WorkoutTracker.Components.Pages;
+
+public partial class EquipmentList : ComponentBase
+{
+    private List<EquipmentEntity> _equipments = new();
+    private DataGrid<EquipmentEntity> _grid = new();
+
+    private List<ColumnDescription<EquipmentEntity>> _columns = new()
+    {
+        new ColumnDescription<EquipmentEntity>()
+        {
+            Name = nameof(EquipmentEntity.Name),
+            Caption = "Name"
+        }
+    };
+
+    [Inject] public EquipmentService EquipmentService { get; set; } = null!;
+
+    [Inject] public DialogService DialogService { get; set; } = null!;
+
+    protected override async Task OnInitializedAsync()
+    {
+        _equipments = await EquipmentService.Get();
+        await base.OnInitializedAsync();
+    }
+
+    private async void AddItem()
+    {
+        var item = new EquipmentEntity();
+        var result = await DialogService.OpenAsync<EquipmentEdit>("Equipment Editor", new Dictionary<string, object>()
+        {
+            ["Model"] = item
+        });
+        if (result == true)
+        {
+            await EquipmentService.Add(item);
+            await _grid.Reload();
+        }
+    }
+
+    private async void DeleteItem(EquipmentEntity item)
+    {
+        await EquipmentService.Remove(item);
+        await _grid.Reload();
+    }
+
+    private async void EditItem(EquipmentEntity item)
+    {
+        var result = await DialogService.OpenAsync<EquipmentEdit>("Equipment Editor", new Dictionary<string, object>()
+        {
+            ["Model"] = item
+        });
+        if (result == true)
+        {
+            await EquipmentService.Update(item);
+            await _grid.Reload();
+        }
+    }
+}
